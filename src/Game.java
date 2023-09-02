63d3ca00d1802cfde956b019a6cad2fc30268ebb
Enrique Barrios <EnriqueB5310@gmail.com>
2023-09-01 22:33:12 -0400

    added display method

--- src/Game.java
+++ src/Game.java
@@ -8,7 +8,7 @@ public class Game {
 
   public void play() throws FileNotFoundException {
     Scanner file = new Scanner(new File("words.txt"));
-    Scanner cin = new Scanner();
+    Scanner cin = new Scanner(System.in);
     List<String> words = new ArrayList<>();
     String word;
 
@@ -23,19 +23,35 @@ word = words.get(ran.nextInt(words.size()));
 System.out.println(word);
 
 List<Character> userguess = new ArrayList<>();
-printWordState(userguess,word);
-GetPlayerGuess(cin, userguess, word);
 
+int wrongCount = 0;
 
+while (true) {
+display(wrongCount);
 
+  if (wrongCount >= 6) {
+    System.out.println("You lost, the word was  " + word);
+    break;
+  }
+
+  printWordState(userguess, word);
+  if(!GetPlayerGuess(cin, userguess, word)) {
+    wrongCount++;
+  };
+  printWordState(userguess, word);
+
+}
 
 
 
 }
 
-  private void GetPlayerGuess(Scanner cin, List<Character> input, String word) {
+  private boolean GetPlayerGuess(Scanner cin, List<Character> input, String word) {
 System.out.println("Enter a letter");
 String letter = cin.nextLine();
+input.add(letter.charAt((0)));
+
+return word.contains(letter);
   }
 
   private void printWordState(List<Character> input, String word) {
@@ -43,14 +59,28 @@ String letter = cin.nextLine();
     for (int i = 0; i < word.length(); i++) {
 
       if(input.contains(word.charAt(i))) {
-        System.out.println(word.charAt(i));
+        System.out.print(word.charAt(i));
       }
 
-      else {System.out.print("-");}
+      else {System.out.print("-");
+      }
 
     }
+System.out.println();
+  }
+
+  private void display(int wrongCount) {
+
+    if(wrongCount >= 1) {System.out.println("0");}
+    if (wrongCount >= 2) {System.out.print("\\");}
+    if (wrongCount >= 3) {System.out.println("/");}
+    if  (wrongCount >= 4) {System.out.println("|");}
+
+
 
   }
+
+
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
         Game game = new Game();
-        game.play();
+        game.plazy();
 
 
     }
